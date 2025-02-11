9986d0c5f981b48eb721c1e1c86c89bdd725ca2c
michguz <mguzz@optusnet.com.au>
2025-02-11 19:12:00 +1100

    Tiny editor broken

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
-import { EditorComponent, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
+import { EditorComponent } from '@tinymce/tinymce-angular';
 
 @Component({
   selector: 'app-text-editor',
-  standalone: true,
+  // standalone: true,
   imports: [EditorComponent],
   templateUrl: './text-editor.component.html',
-  styleUrl: './text-editor.component.css',
-  providers: [
-    { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
-  ]
+  styleUrl: './text-editor.component.css'
 })
 export class TextEditorComponent {
   init: EditorComponent['init'] = {
     plugins: 'lists link image table code help wordcount',
-    base_url: '/timymce',
-    suffix: '.min'
+    // base_url: '/timymce',
+    // suffix: '.min'
   };
 
 }
