70a0c531b6a30ba9b05a9ba075c49315a61ffc0b
michguz <mguzz@optusnet.com.au>
2025-02-11 20:55:37 +1100

    Fixed text editor but with API key block

--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 @Component({
   selector: 'app-text-editor',
-  imports: [],
+  imports: [EditorModule],
   templateUrl: './text-editor.component.html',
   styleUrl: './text-editor.component.css'
 })
 export class TextEditorComponent {
+  editorValue: string = '';
+
+  editorConfig = {
+    height: 500,
+    menubar: false,
+    plugins: ['advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'print', 'preview', 'searchreplace', 'wordcount'],
+    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | outdent indent | link image'
+  }
 }
