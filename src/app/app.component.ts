8ad95cbb739655d7975cf5424b0b432d4c9bd2b3
michguz <mguzz@optusnet.com.au>
2025-02-11 19:03:24 +1100

    Added text editior tinymce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  
 }
--- src/app/text-editor/text-editor.component.ts
+++ src/app/text-editor/text-editor.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { EditorComponent, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 
 @Component({
   selector: 'app-text-editor',
-  imports: [],
+  standalone: true,
+  imports: [EditorComponent],
   templateUrl: './text-editor.component.html',
-  styleUrl: './text-editor.component.css'
+  styleUrl: './text-editor.component.css',
+  providers: [
+    { provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }
+  ]
 })
 export class TextEditorComponent {
+  init: EditorComponent['init'] = {
+    plugins: 'lists link image table code help wordcount',
+    base_url: '/timymce',
+    suffix: '.min'
+  };
 
 }
